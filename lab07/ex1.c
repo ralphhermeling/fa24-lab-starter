bea3609d997d2227f12f35f2c5039a4f04a7e480
Ralph Hermeling <ralphhermeling@hotmail.com>
2025-04-04 16:08:18 +0200

    Finish lab 7

--- lab07/ex1.c
+++ lab07/ex1.c
@@ -1,3 +1,4 @@
+#include <emmintrin.h>
 #include <time.h>
 #include <stdio.h>
 #include <x86intrin.h>
@@ -52,10 +53,27 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        /* YOUR CODE GOES HERE */
+        __m128i simd_sum = _mm_setzero_si128();
+        for(unsigned int i = 0; i + 4 <= NUM_ELEMS; i += 4){
+            __m128i v = _mm_loadu_si128((__m128i*)(&vals[i]));
+            __m128i mask = _mm_cmpgt_epi32(v, _127);
+            __m128i filtered = _mm_and_si128(v,mask);
+            simd_sum = _mm_add_epi32(simd_sum, filtered);
+        }
 
-        /* Hint: you'll need a tail case. */
+        int temp[4];
+        _mm_storeu_si128((__m128i*)temp, simd_sum);
+        for(int j = 0; j < 4; j++){
+            result += temp[j];
+        }
+
+        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++){
+            if(vals[i] >= 128){
+                result += vals[i];
+            }
+        }
     }
+
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
     clock_t end = clock();
@@ -70,6 +88,40 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        __m128i simd_sum = _mm_setzero_si128();
+        for(unsigned int i = 0; i + 4 * 4 <= NUM_ELEMS; i += 4 * 4){
+            __m128i v = _mm_loadu_si128((__m128i*)(&vals[i]));
+            __m128i mask = _mm_cmpgt_epi32(v, _127);
+            __m128i filtered = _mm_and_si128(v,mask);
+            simd_sum = _mm_add_epi32(simd_sum, filtered);
+
+            v = _mm_loadu_si128((__m128i*)(&vals[i+4]));
+            mask = _mm_cmpgt_epi32(v, _127);
+            filtered = _mm_and_si128(v,mask);
+            simd_sum = _mm_add_epi32(simd_sum, filtered);
+
+            v = _mm_loadu_si128((__m128i*)(&vals[i+8]));
+            mask = _mm_cmpgt_epi32(v, _127);
+            filtered = _mm_and_si128(v,mask);
+            simd_sum = _mm_add_epi32(simd_sum, filtered);
+
+            v = _mm_loadu_si128((__m128i*)(&vals[i+12]));
+            mask = _mm_cmpgt_epi32(v, _127);
+            filtered = _mm_and_si128(v,mask);
+            simd_sum = _mm_add_epi32(simd_sum, filtered);
+        }
+
+        int temp[4];
+        _mm_storeu_si128((__m128i*)temp, simd_sum);
+        for(int j = 0; j < 4; j++){
+            result += temp[j];
+        }
+
+        for(unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++){
+            if(vals[i] >= 128){
+                result += vals[i];
+            }
+        }
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
 
@@ -80,4 +132,4 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     clock_t end = clock();
     printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
     return result;
-}
+} 
--- lab07/ex2.c
+++ lab07/ex2.c
@@ -1,4 +1,5 @@
 #include "ex2.h"
+#include <omp.h>
 
 double dotp_naive(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
@@ -10,23 +11,43 @@ double dotp_naive(double* x, double* y, int arr_size) {
 // Critical Keyword
 double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
-    // Use the critical keyword here!
+    #pragma omp parallel for 
+    for(int i = 0; i < arr_size; i++){
+        double dotp = x[i] * y[i];
+
+        #pragma omp critical
+        {
+            global_sum += dotp;
+        }
+    }
     return global_sum;
 }
 
 // Reduction Keyword
 double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
-    // Use the reduction keyword here!
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++)
+        global_sum += x[i] * y[i];
     return global_sum;
 }
 
 // Manual Reduction
 double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
-    // Do NOT use the `reduction` directive here!
+    #pragma omp parallel  
+    {
+        double local_sum = 0;
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++){
+            local_sum+=x[i] * y[i];
+        }
+
+        #pragma omp critical 
+        {
+            global_sum += local_sum;
+        }
+    }
+
     return global_sum;
 }
